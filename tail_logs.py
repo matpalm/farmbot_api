af1ee3c0d07f8b5a30bfadefd96479895e0130df
Mat Kelcey <matthew.kelcey@gmail.com>
2018-11-18 22:30:25 +1100

    tail logs

---
+++ tail_logs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import creds
+import paho.mqtt.client as mqtt
+
+def on_connect(client, userdata, flags, rc):
+  # occasional status message
+  client.subscribe("bot/" + creds.device_id + "/status")
+  # log entries
+  client.subscribe("bot/" + creds.device_id + "/logs")
+  # request commands from client
+  client.subscribe("bot/" + creds.device_id + "/from_clients")
+  # responses
+  client.subscribe("bot/" + creds.device_id + "/from_device")
+
+def on_message(client, userdata, msg):
+  print(msg.topic + " " + str(msg.payload))
+
+client = mqtt.Client()
+client.username_pw_set(creds.device_id, creds.token)
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect("brisk-bear.rmq.cloudamqp.com", 1883, 60)
+client.loop_forever()
