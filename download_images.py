3e0dcb83a4f789f1296102e2c2ff444bd6fed01d
Mat Kelcey <matthew.kelcey@gmail.com>
2018-11-21 15:08:42 +1100

    make download images sync to local db

--- download_images.py
+++ download_images.py
@@ -4,14 +4,18 @@ import os
 import requests
 import json
 import creds
+from image_db import ImageDB
+from dateutil.parser import parse
+from datetime import timezone
+
+REQUEST_HEADERS = {'Authorization': 'Bearer ' + creds.token, 'content-type': "application/json"}
 
 # keep fetching all images until there are none with an attachment of "placeholder". this
 # can occur if we call this too quickly after getting an ack for a photo
 retry = True
 while retry:
   retry = False  # assume ok unless we see a placeholder
-  headers = {'Authorization': 'Bearer ' + creds.token, 'content-type': "application/json"}
-  response = requests.get('https://my.farmbot.io/api/images', headers=headers)
+  response = requests.get('https://my.farmbot.io/api/images', headers=REQUEST_HEADERS)
   images = response.json()
   for image in images:
     if 'placehold.it' in image['attachment_url']:
@@ -21,13 +25,27 @@ while retry:
       retry = True
       break
 
-for image in images:
-  captured_img_name = image['meta']['name']
+image_db = ImageDB()
+image_db.create_if_required()
+
+for image_info in images:
+  print(image_info)
+
+  # convert date time of capture from UTC To AEDT and extract
+  # a simple string version for local image filename
+  dts = parse(image_info['attachment_processed_at'])
+  dts = dts.replace(tzinfo=timezone.utc).astimezone(tz=None)
+  local_img_name = "imgs/%s.jpg" % dts.strftime("%Y%m%d_%H%M%S")
+
+  # download image from google storage and save locally
+  captured_img_name = image_info['meta']['name']
   if captured_img_name.startswith("/tmp/images"):
-    img_name = "imgs/" + captured_img_name.replace("/tmp/images/", "") + ".jpg"
-    if os.path.isfile(img_name):
-      print(img_name, "*")
-    else:
-      print(img_name, image['attachment_url'])
-      req = requests.get(image['attachment_url'], allow_redirects=True)
-      open(img_name, 'wb').write(req.content)
+    req = requests.get(image_info['attachment_url'], allow_redirects=True)
+    open(local_img_name, 'wb').write(req.content)
+
+  # add entry to db
+  image_db.insert(image_info, dts, local_img_name)
+
+  # post delete from cloud storage
+  response = requests.delete("https://my.farmbot.io/api/images/%d" % image_info['id'],
+                             headers=REQUEST_HEADERS)
---
+++ image_db.py
@@ -0,0 +1,84 @@
+# image db helper
+
+import sqlite3
+import json
+
+class ImageDB(object):
+
+  def __init__(self, image_db_file='image.db', check_same_thread=True):
+    self.conn = sqlite3.connect(image_db_file, check_same_thread=check_same_thread)
+
+  def create_if_required(self):
+    # called once to create db
+      c = self.conn.cursor()
+      try:
+        c.execute('''create table imgs (
+                          id integer primary key autoincrement,
+                          farmbot_id integer,
+                          capture_time text,
+                          x integer,
+                          y integer,
+                          z integer,
+                          api_response text,
+                          filename text
+                     )''')
+      except sqlite3.OperationalError:
+        # assume table already exists? clumsy...
+        pass
+
+#  def has_been_created(self):
+#    c = self.conn.cursor()
+#    c.execute("select name from sqlite_master where type='table' AND name='imgs';")
+#    return c.fetchone() is not None
+
+  def insert(self, api_response, dts, filename):
+    farmbot_id = api_response['id']
+    capture_time = dts.strftime("%Y-%m-%d %H:%M:%S")
+    x, y, z = map(int, [api_response['meta'][c] for c in ['x', 'y', 'z']])
+    c = self.conn.cursor()
+    insert_values = (farmbot_id, capture_time, x, y, z, json.dumps(api_response), filename)
+#    print("I", insert_values)
+    c.execute("insert into imgs (farmbot_id, capture_time, x, y, z, api_response, filename) values (?,?,?,?,?,?,?)", insert_values)
+    self.conn.commit()
+
+
+
+#  def _id_for_img(self, img):
+#    c = self.conn.cursor()
+#    c.execute("select id from imgs where filename=?", (img,))
+#    id = c.fetchone()
+#    if id is None:
+#      return None
+#    else:
+#      return id[0]
+
+#  def _create_row_for_img(self, img):
+#    c = self.conn.cursor()
+#    c.execute("insert into imgs (filename) values (?)", (img,))
+#    self.conn.commit()
+#    return self._id_for_img(img)
+
+#  def _delete_labels_for_img_id(self, img_id):
+#    c = self.conn.cursor()
+#    c.execute("delete from labels where img_id=?", (img_id,))
+#    self.conn.commit()
+
+#  def _add_rows_for_labels(self, img_id, labels, flip=False):
+#    c = self.conn.cursor()
+#    for x, y in labels:
+#      if flip:
+#        # TODO: DANGER WILL ROBERTSON! the existence of this, for the population
+#        #       of db from centroids_of_connected_components denotes some inconsistency
+#        #       somewhere... :/
+#        x, y = y, x
+#      c.execute("insert into labels (img_id, x, y) values (?, ?, ?)", (img_id, x, y,))
+#    self.conn.commit()
+
+
+#if __name__ == "__main__":
+#  import argparse
+#  parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+#  parser.add_argument('--label-db', type=str, default="label.db")
+#  opts = parser.parse_args()
+#  db = LabelDB(label_db_file=opts.label_db)
+#  print("\n".join(db.imgs()))
